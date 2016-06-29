9d28e4d28b5d97886ea0ad9648801a7d48c2faff
Vince <876753183@qq.com>
2016-06-29 18:38:46 +0800

    init project

---
+++ js/common/main.js
@@ -0,0 +1,17 @@
+/**
+ * === Javascript 主控制器 模块 ======================
+ * Copyright (c) 2016 YangCheung, All rights reserved.
+ * http://yangcheung.github.com
+ * 
+ * @version 1.0
+ * @author  YangCheung <876753183@qq.com>
+ * @description CSS3LibCtrl页面第一个controller层 
+ * ---2016-6-29 ---------------------------------------
+ */
+
+/**
+ * 
+ */
+Young.controller('CSS3LibCtrl',function(){
+
+});
